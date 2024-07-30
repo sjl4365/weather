b79f379d6cb7ef5895ac5a78b127dd65619f3781
SL <sl@sls-laptop.home>
2024-07-30 10:37:36 -0400

    feature signup working

---
+++ src/api.js
@@ -0,0 +1,10 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '076bcda108msh0515c6885a73282p1100a6jsneba8351e732e',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const Geo_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
